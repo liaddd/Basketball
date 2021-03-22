c2e750ec61cbc6acb8a19ee152c7c9167dc7a8df
liaddd <liaddd@gmail.com>
2021-03-22 13:17:24 +0200

    first commit

---
+++ Assets/Project/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public Player player;
+    public float resetTimer = 5f;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (!player.holdingBall)
+        {
+            resetTimer -= Time.deltaTime;
+            if(resetTimer <= 0)
+            {
+                SceneManager.LoadScene("Game");
+            }
+        }
+        
+    }
+}
---
+++ Assets/Project/Scripts/ScoreArea.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreArea : MonoBehaviour
+{
+
+    public GameObject effectObject;
+
+    private void Start()
+    {
+        effectObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Ball>() != null)
+        {
+            effectObject.SetActive(true);
+        }
+    }
+}
